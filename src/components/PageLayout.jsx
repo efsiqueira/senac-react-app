3c7078970a2384e235f68f2aa80271d1f104cf90
Eduardo <edu.fss14@gmail.com>
2022-10-14 22:01:09 -0300

    criação do Menu

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,13 +1,76 @@
-import { Link, Outlet } from 'react-router-dom'
+import { AppBar, Box, Drawer, IconButton, List, ListItem, ListItemButton, ListItemText, Toolbar, Typography } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+import MenuIcon from '@mui/icons-material/Menu'
+import { useState } from 'react'
 
-export const PageLayout = () => (
-  <>
-    <nav>
-      <Link to={'/'}>Home</Link>
-      <Link to={'/sobre'}>Sobre</Link>
-      <Link to={'/noticias'}>Notícias</Link>
-      <Link to={'/contato'}>Contato</Link>
-    </nav>
-    <Outlet />
-  </>
-)
+export const PageLayout = () => {
+
+  const DrawerApp = () => (
+    <Box>
+      <Typography variant="h5" >
+        Meu menu
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemButton component="a" href="/">
+            <ListItemText primary={"Home"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="/sobre">
+            <ListItemText primary={"Sobre"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="/noticias">
+            <ListItemText primary={"Notícias"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="/contato">
+            <ListItemText primary={"Contato"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  )
+    
+  const ContainerMenu = () => {
+    const [aberto, setAberto] = useState(false)
+
+    const comportamentoMenu = () => {
+      setAberto(!aberto)
+    }
+
+    return (
+      <Box>
+        <AppBar>
+          <Toolbar>
+            <IconButton
+              onClick={comportamentoMenu}
+            >
+              <MenuIcon></MenuIcon>
+            </IconButton>
+          </Toolbar>              
+        </AppBar>
+        <Box>
+          <Drawer
+            open={aberto}
+            onClose={comportamentoMenu}
+          >
+            <DrawerApp />
+          </Drawer>
+        </Box>
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      <ContainerMenu />
+      <Box style={{ marginTop: "80px" }}>
+        <Outlet />
+      </Box>
+    </>
+  )
+}
